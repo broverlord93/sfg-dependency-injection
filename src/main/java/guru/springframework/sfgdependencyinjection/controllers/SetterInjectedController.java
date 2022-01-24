043bdb1719583a52918fd43770b943e5a0be5368
Anthony Limani <alimani9193@gmail.com>
2022-01-24 16:37:55 -0500

    Manual dependency Injection using PropertyInjectedController, SetterInjectedController, and ConstructorInjectedController

---
+++ src/main/java/guru/springframework/sfgdependencyinjection/controllers/SetterInjectedController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgdependencyinjection.controllers;
+
+import guru.springframework.sfgdependencyinjection.services.GreetingService;
+
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+
+}
