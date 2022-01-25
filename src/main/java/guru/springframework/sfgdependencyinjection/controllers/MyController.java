99ecfa437e84972908b0741aabfd71e745c9a135
Anthony Limani <alimani9193@gmail.com>
2022-01-25 07:34:48 -0500

    Created a Primary Bean for use in MyController

--- src/main/java/guru/springframework/sfgdependencyinjection/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdependencyinjection/controllers/MyController.java
@@ -1,12 +1,18 @@
 package guru.springframework.sfgdependencyinjection.controllers;
 
+import guru.springframework.sfgdependencyinjection.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello");
-        return "Hello";
+        return greetingService.sayGreeting();
     }
 }
