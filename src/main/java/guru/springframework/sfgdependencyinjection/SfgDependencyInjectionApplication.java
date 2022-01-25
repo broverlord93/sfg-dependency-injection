ce9562ceac3630c43cef5c6ced489c3e0570422f
Anthony Limani <alimani9193@gmail.com>
2022-01-25 07:49:48 -0500

    Added a Profile for EN and ES, modified pplication.properties to add an active Profile, consumes a different Greeting Service bean depending on profile

--- src/main/java/guru/springframework/sfgdependencyinjection/SfgDependencyInjectionApplication.java
+++ src/main/java/guru/springframework/sfgdependencyinjection/SfgDependencyInjectionApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdependencyinjection;
 
-import guru.springframework.sfgdependencyinjection.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdependencyinjection.controllers.MyController;
-import guru.springframework.sfgdependencyinjection.controllers.PropertyInjectedController;
-import guru.springframework.sfgdependencyinjection.controllers.SetterInjectedController;
+import guru.springframework.sfgdependencyinjection.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +13,10 @@ public class SfgDependencyInjectionApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDependencyInjectionApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+
+		System.out.println(i18nController.sayHello());
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
