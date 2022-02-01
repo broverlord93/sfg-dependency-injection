27ee4b945896f01954cf82cff9bfba75c47bc5b6
Anthony Limani <alimani9193@gmail.com>
2022-02-01 07:51:06 -0500

    Added config for english and spanish greeting services

--- src/main/java/guru/springframework/sfgdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdependencyinjection/config/GreetingServiceConfig.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfgdependencyinjection.config;
 
-import guru.springframework.sfgdependencyinjection.services.ConstructorGreetingService;
-import guru.springframework.sfgdependencyinjection.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdependencyinjection.services.SetterInjectedGreetingService;
+import guru.springframework.sfgdependencyinjection.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService(){
@@ -22,5 +28,17 @@ public class GreetingServiceConfig {
     @Bean
     SetterInjectedGreetingService setterInjectedGreetingService(){
         return new SetterInjectedGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService(){
+        return new I18nSpanishGreetingService();
     }
 }
